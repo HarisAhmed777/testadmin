d876f62e40d540dac98f75a68d758487c30826b4
HarisAhmed777 <ahmedharis371@gmail.com>
2024-07-18 12:30:55 +0530

    style:admin

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,7 @@ function Menu(){
 <aside className="main-sidebar sidebar-dark-primary elevation-4">
   <a href="" className="brand-link">
     <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-    <span className="brand-text font-weight-light">AdminLTE 3</span>
+    <span className="brand-text font-weight-light">Star Holidays</span>
   </a>
   <div className="sidebar">
     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
@@ -15,7 +15,7 @@ function Menu(){
         <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
       </div>
       <div className="info">
-        <a href="" className="d-block">Alexander Pierce</a>
+        <a href="" className="d-block">Admin</a>
       </div>
     </div>
     <div className="form-inline">
