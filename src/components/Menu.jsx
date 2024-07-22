25501e35a088e33c0dfabaf58771f312676fb63f
HarisAhmed777 <ahmedharis371@gmail.com>
2024-07-22 13:27:59 +0530

    corrections in sidebar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import Logo from '../../public/Logo.png';
 
 function Menu() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <>
-      <aside className="main-sidebar sidebar-dark-primary elevation-4 custom-sidebar">
-        <a href="" className="brand-link">
-          <img src={Logo} alt="Star Holidays Logo" className="brand-image" style={{ opacity: '8', height: '50px', width: '50px' }} />
+      <aside className={`main-sidebar sidebar-dark-primary  custom-sidebar ${isSidebarOpen ? 'active' : ''}`}>
+        <Link to="/" className="brand-link">
+          <img src={Logo} alt="Star Holidays Logo" className="brand-image" style={{ opacity: '0.8', height: '50px', width: '50px' }} />
           <span className="brand-text font-weight-light">Star Holidays</span>
-        </a>
+        </Link>
         <div className="sidebar">
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="info">
-              <a href="" className="d-block">Admin</a>
+              <Link to="/" className="d-block">Admin</Link>
             </div>
           </div>
           <div className="form-inline">
@@ -27,107 +33,56 @@ function Menu() {
             </div>
           </div>
           <nav className="mt-2">
-            <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" data-accordion="false">
-              <li className="">
-                <a href="/" className="nav-link active lii">
-                  <i className="fas fa-tachometer-alt" />
-                  <p className="ms-2">
-                    Dashboard
-                  </p>
-                </a>
-              </li>
-              {/* <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <i className="nav-icon fas fa-chart-pie" />
-                  <p className="ms-2">
-                    Charts
-                    <i className="right fas fa-angle-left" />
-                  </p>
-                </a>
-                <ul className="nav nav-treeview">
-                  <li className="nav-item">
-                    <a href="" className="nav-link">
-                      <i className="far fa-circle nav-icon" />
-                      <p>ChartJS</p>
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a href="" className="nav-link">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Flot</p>
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a href="" className="nav-link">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Inline</p>
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a href="" className="nav-link">
-                      <i className="far fa-circle nav-icon" />
-                      <p>uPlot</p>
-                    </a>
-                  </li>
-                </ul>
-              </li> */}
+            <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
               <li className="nav-item">
-                <a href="#" className="nav-link">
+                <Link to="/" className="nav-link active lii">
+                  <i className="fas fa-tachometer-alt" />
+                  <p className="ms-2">Dashboard</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="#" className="nav-link">
                   <i className="nav-icon fas fa-table" />
                   <p className="ms-2">
                     DataTables
-                    <i className="fas fa-angle-left right" />
+                    <i className="right fas fa-angle-left" />
                   </p>
-                </a>
+                </Link>
                 <ul className="nav nav-treeview">
-                  <Link to ='/allbookings'>
                   <li className="nav-item">
-                    <a href="" className="nav-link">
+                    <Link to="/allbookings" className="nav-link">
                       <i className="far fa-circle nav-icon" />
                       <p>BookingsTable</p>
-                    </a>
+                    </Link>
                   </li>
-                  </Link>
-                  <Link to='/allusers'>
-                    <li className="nav-item">
-                      <a href="" className="nav-link">
-                        <i className="far fa-circle nav-icon" />
-                        <p>UsersTable</p>
-                      </a>
-                    </li>
-                  </Link>
-                  <Link to = '/allforms'>
                   <li className="nav-item">
-                    <a href="" className="nav-link">
+                    <Link to="/allusers" className="nav-link">
                       <i className="far fa-circle nav-icon" />
-                      <p>Form request Table</p>
-                    </a>
+                      <p>UsersTable</p>
+                    </Link>
                   </li>
-                  </Link>
+                  <li className="nav-item">
+                    <Link to="/allforms" className="nav-link">
+                      <i className="far fa-circle nav-icon" />
+                      <p>Form Request Table</p>
+                    </Link>
+                  </li>
                 </ul>
               </li>
-              <Link to='/calenderpage'>
-                <li className="">
-                  <a href="" className="nav-link active lii">
-                    <i className="fas fa-tachometer-alt" />
-                    <p className="ms-2">
-                      Calendar
-                    </p>
-                  </a>
-                </li>
-              </Link>
-              <Link to='/offers'>
-                <li className="">
-                  <a href="/offers" className="nav-link active lii">
-                    <i className="fas fa-tachometer-alt" />
-                    <p className="ms-2">
-                      Offers
-                    </p>
-                  </a>
-                </li>
-              </Link>
+              <li className="nav-item">
+                <Link to="/calendarpage" className="nav-link">
+                  <i className="fas fa-calendar-alt" />
+                  <p className="ms-2">Calendar</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/offers" className="nav-link">
+                  <i className="fas fa-tags" />
+                  <p className="ms-2">Offers</p>
+                </Link>
+              </li>
             </ul>
-            <button className="btn btn-sidebar-toggle" data-widget="pushmenu">
+            <button className="btn btn-sidebar-toggle" onClick={toggleSidebar}>
               <i className="fas fa-times" />
             </button>
           </nav>
